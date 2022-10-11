03bcfd4e8edc7f54e7a178c87363281b43769ce9
lowlyphe <jones.kyle2893@gmail.com>
2022-10-11 13:23:30 -0400

    made client and server talk

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,17 +1,27 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 # import folium
 # from folium.plugins import MarketCluster
 # import pandas as pd
 
 app = Flask(__name__)
+CORS(app, support_credentials=True)
 
 # Google maps API
 
 
 @app.route("/api/leaflet", methods=['GET', 'POST'])
+@cross_origin(supports_credentials=True)
 def leaflet():
-    data = request
-    print(data)
+    data = request.get_json()
+    location = data['location']
+    print(location)
+    return jsonify({'result': 'Success!'})
+
+
+@app.route('/api/test')
+def test():
+    return {"test": "test"}
 
 
 if __name__ == "__main__":
--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,13 +1,28 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import axios from 'axios'
 
 export default function Maps() {
 
     let searchRef = useRef()
-    let search = searchRef.current.value
+
+    // useEffect(async () => {
+    //   async function fetchData() {
+    //     const data = await fetch('/api/test')
+    //     console.log(data)
+    //   }
+    //   fetchData()
+    // })
 
     const handleSearch = () => {
-        axios.post('https://localhost:5000/api/leaflet', {location: search})
+      let search = searchRef.current.value
+      const requestOptions = {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({location: search})
+      }
+      fetch('http://localhost:5000/api/leaflet', requestOptions)
+      .then(res => res.json())
+      .then(res => console.log(res))
     }
 
   return (
